770364fdb70c1e13493e3e0986072e415e9bdc61
olga.kovaleva <olga.kovaleva.9595@gmail.com>
2023-11-16 15:10:44 +0100

    homeTask_11: methods return types were removed for ObjectManipulator class

--- homeTask_11/src/task_2/objectManipulator.ts
+++ homeTask_11/src/task_2/objectManipulator.ts
@@ -8,21 +8,21 @@ interface ObjectManipulatorInterface<T> {
 export default class ObjectManipulator<T> implements ObjectManipulatorInterface<T> {
   constructor(protected obj: T) {}
 
-  public set<K extends keyof T>(key: K, value: T[K]): ObjectManipulatorInterface<T> {
+  public set<K extends keyof T>(key: K, value: T[K]) {
     return new ObjectManipulator({ ...this.obj, [key]: value });
   }
 
-  public get<K extends keyof T>(key: K): T[K] | undefined {
+  public get<K extends keyof T>(key: K) {
     return this.obj[key];
   }
 
-  public delete<K extends keyof T>(key: K): ObjectManipulatorInterface<T> {
+  public delete<K extends keyof T>(key: K) {
     const newObj = { ...this.obj };
     delete newObj[key];
     return new ObjectManipulator(newObj);
   }
 
-  public getObject(): T {
+  public getObject() {
     return this.obj;
   }
 }
