4987b5058e93ceaaa62cdbf7168baa341ca62814
olga.kovaleva <olga.kovaleva.9595@gmail.com>
2023-12-26 12:51:12 +0100

    homeTask_18: e2e test creation using Playwright

---
+++ homeTask_18/playwright.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  reporter: 'html',
+  use: {
+    baseURL: 'https://playwright.dev/',
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    headless: true,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
